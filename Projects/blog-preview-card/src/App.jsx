4ee6e992792982ca2314bcdb0c7ff59ecd0384d6
JSDK <bewjsdk41@gmail.com>
2024-03-18 21:07:45 +0700

    Blog Preview Card V1.0.0

--- Projects/blog-preview-card/src/App.jsx
+++ Projects/blog-preview-card/src/App.jsx
@@ -1,5 +1,5 @@
+import './styles/App.scss';
 import BlogCard from "./components/BlogCard"
-
 
 function App() {
 
--- Projects/blog-preview-card/src/components/Content.jsx
+++ Projects/blog-preview-card/src/components/Content.jsx
@@ -2,11 +2,10 @@ function Content() {
   return (
     <div className="blog-card-content">
       <button className="btn-tag">Learning</button>
-      <p>
-        Published 
-        <span className="time-info">21 Dec 2023</span>
-      </p>
-      <h1>HTML & CSS foundations</h1>
+      <p>Published 21 Dec 2023</p>
+      <a href="#" aria-label="Link to HTML and CSS Foundation">
+        <h1>HTML & CSS foundations</h1>
+      </a>
       <p>
         These languages are the backbone of every website, 
         defining structure, content, and presentation.
